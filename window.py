8f9305d2544a8214cb5c0dee278db52798be64d2
tfriezz <roswall.tim@gmail.com>
2025-06-06 12:34:30 +0900

    add point, line, window.draw_line

--- window.py
+++ window.py
@@ -22,11 +22,44 @@ class Window:
     def close(self):
         self.running = False
 
-    #   self.__root.protocol()
+    def draw_line(self, line_instance, fill_color):
+        line_instance.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas_instance, fill_color=""):
+        self.canvas_instance = canvas_instance
+        self.fill_color = fill_color
+        canvas_instance.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill=fill_color,
+            width=2,
+        )
 
 
 def main():
     win = Window(800, 600)
+    my_point_1 = Point(30, 400)
+    my_point_2 = Point(55, 255)
+    my_line_1 = Line(my_point_1, my_point_2)
+    my_point_3 = Point(10, 500)
+    my_point_4 = Point(500, 600)
+    my_line_2 = Line(my_point_3, my_point_4)
+    win.draw_line(my_line_1, "red")
+    win.draw_line(my_line_2, "blue")
     win.wait_for_close()
 
 
